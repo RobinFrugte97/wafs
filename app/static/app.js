18bd197fb1cc19e169c0d9ad97c4258c22b3c4dc
RobinFrugte97 <robin.frugte@hotmail.com>
2018-02-21 15:27:46 +0100

    Update app.js
    Final version of JS

--- app/static/app.js
+++ app/static/app.js
@@ -1,10 +1,21 @@
 //IIFE immediately invoked function expression
 (function(){
-
+  'use strict';
   var app = {
     init: function(){
       console.log("app initialised");
       routes.init();
+    }
+  }
+
+  var events = {
+    filter: function() {
+      var submit = document.getElementById("submit");
+      submit.addEventListener("click", function(){
+          var userInput = document.getElementById("filterInput").value;
+          window.location.replace("#filter/" + userInput);
+      })
+
     }
   }
   //Handle routes & states
@@ -12,7 +23,7 @@
     init: function(){
       this.handleEvents();
     },
-    handleEvents: function(){
+    handleEvents: function(data){
       routie({
         "characters/:id": function(id){
           var sections = document.querySelectorAll("section");
@@ -20,6 +31,10 @@
             section.classList.toggle('active');
           });
           api.character(id);
+        },
+        'filter/:input': function(input){
+          console.log(input);
+          api.filter(input);
         },
         '*': function() {
           var sections = document.querySelectorAll("section");
@@ -33,12 +48,13 @@
   }
   //Render / toggle sections
   var template = {
-    render: function(compressedData, type){
+    render: function(data, type){
       if (type == 'list') {
-        var renderData = compressedData.map(function(d, i){
+        var renderData = data.map(function(d, i){
           return {
             character: d.name,
-            id: d.id
+            id: d.id,
+            thumbnail: d.thumbnail.path + '/standard_xlarge.jpg'
           }
         });
         var directives = {
@@ -46,20 +62,26 @@
             href: function() {
               return `#characters/${this.id}`;
             }
-          }
-        }
-        Transparency.render(document.getElementById("characters"), renderData, directives);
-      } else if (type == 'detail') {
-        var renderData = compressedData.map(function(d, i){
-          return {
-            character: d.name,
-            thumbnail: d.thumbnail.path + '/landscape_incredible.jpg'
-          }
-        });
-        var directives = {
+          },
           thumbnail: {
             src: function(){
               return this.thumbnail;
+            }
+          }
+        }
+        Transparency.render(document.getElementById("characters"), renderData, directives);
+        events.filter();
+      } else if (type == 'detail') {
+        var renderData = data.map(function(d, i){
+          return {
+            character: d.name,
+            heroImage: d.thumbnail.path + '/portrait_uncanny.jpg'
+          }
+        });
+        var directives = {
+          heroImage: {
+            src: function(){
+              return this.heroImage;
             }
           }
         }
@@ -68,46 +90,56 @@
     }
   }
 
-  var collection = {
-    allStories: function(data){
-
+  var feature = {
+    init: function(data, input){
+      this.filter(data, input);
     },
-    filter: function(data, keyword) {
-      data.filter(function(item){
-        return item.title.contains(keyword);
-      })
+    filter: function(data, input) {
+      var filterCharacter = data.filter(function(data){
+        return data.name == input;
+      });
+      console.log(filterCharacter);
+      var type = "list";
+      template.render(filterCharacter, type);
     }
   }
 
   var api = {
-    retrieve (url, type) {
+    retrieve (url, type, userInput) {
         var request = new XMLHttpRequest();
         request.open('GET', `${url}` , true);
         request.onload = function() {
           if (request.status >= 200 && request.status < 400) {
-           // Success!
-            var data = JSON.parse(request.responseText);
-            var compressedData = data.data.results;
-            console.log(type);
-            template.render(compressedData, type);
+            if (type !== 'filter') {
+              var data = JSON.parse(request.responseText);
+              var compressedData = data.data.results;
+              console.log(type);
+              template.render(compressedData, type);
+            } else {
+              var data = JSON.parse(request.responseText);
+              var compressedData = data.data.results;
+              console.log('api' + userInput);
+              feature.init(compressedData, userInput);
+            }
+
           } else {
             console.log(request.status);
-         // We reached our target server, but it returned an error
           }
         };
         request.onerror = function() {
           console.log('error');
-         // There was a connection error of some sort
         };
         request.send();
     },
     characters () {
-      this.retrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=1', 'list')
+      this.retrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=100', 'list')
     },
     character (id) {
       this.retrieve(`http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05`, 'detail')
+    },
+    filter (userInput) {
+      this.retrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=100', 'filter', userInput)
     }
   }
   app.init();
 })()
-
