825f41c2d3b0f775cae6a7694293442f27372c18
RobinFrugte97 <robin.frugte@hotmail.com>
2018-02-23 13:52:00 +0100

    Update app.js

--- app/static/app.js
+++ app/static/app.js
@@ -10,11 +10,27 @@
 
   var events = {
     filter: function() {
-      var submit = document.getElementById("submit");
-      submit.addEventListener("click", function(){
-          var userInput = document.getElementById("filterInput").value;
-          window.location.replace("#filter/" + userInput);
-      })
+      var filterInput = document.getElementById("filterInput");
+      var filteredCharacters = []
+      filterInput.onkeyup = function(e){
+        var value = e.currentTarget.value;
+        console.log(value);
+        filteredCharacters = [];
+
+        api.test.forEach(function(data){
+          var name = data.name.indexOf(value);
+          if (name > -1) {
+            filteredCharacters.push(data);
+            var type = "list";
+            template.render(filteredCharacters, type);
+          }
+        });
+      }
+      // var submit = document.getElementById("submit");
+      // submit.addEventListener("click", function(){
+      //     var userInput = document.getElementById("filterInput").value;
+      //     window.location.replace("#filter/" + userInput);
+      // })
 
     }
   }
@@ -26,22 +42,25 @@
     handleEvents: function(data){
       routie({
         "characters/:id": function(id){
+          api.character(id);
           var sections = document.querySelectorAll("section");
           sections.forEach(function(section){
             section.classList.toggle('active');
           });
-          api.character(id);
+
         },
         'filter/:input': function(input){
           console.log(input);
           api.filter(input);
         },
         '*': function() {
+          api.characters();
           var sections = document.querySelectorAll("section");
           sections.forEach(function(section){
             section.classList.toggle('active');
           });
-          api.characters();
+
+
         }
       });
     }
@@ -75,7 +94,8 @@
         var renderData = data.map(function(d, i){
           return {
             character: d.name,
-            heroImage: d.thumbnail.path + '/portrait_uncanny.jpg'
+            heroImage: d.thumbnail.path + '/portrait_uncanny.jpg',
+            comics: d.comics.items
           }
         });
         var directives = {
@@ -90,38 +110,70 @@
     }
   }
 
-  var feature = {
-    init: function(data, input){
-      this.filter(data, input);
-    },
-    filter: function(data, input) {
-      var filterCharacter = data.filter(function(data){
-        return data.name == input;
-      });
-      console.log(filterCharacter);
-      var type = "list";
-      template.render(filterCharacter, type);
+  var helper = {
+    loader: {
+      show: function(){
+        console.log("show");
+        var loader = document.getElementById("wrapper");
+        loader.classList.remove("hidden");
+        loader.classList.add("flex");
+      },
+      hide: function(){
+        console.log("hide");
+        var loader = document.getElementById("wrapper");
+        loader.classList.remove("flex");
+        loader.classList.add("hidden");
+      }
     }
   }
 
   var api = {
-    retrieve (url, type, userInput) {
+    test: [],
+    retrieve: function (url, type, userInput) {
+      helper.loader.show();
         var request = new XMLHttpRequest();
         request.open('GET', `${url}` , true);
         request.onload = function() {
           if (request.status >= 200 && request.status < 400) {
-            if (type !== 'filter') {
               var data = JSON.parse(request.responseText);
               var compressedData = data.data.results;
               console.log(type);
               template.render(compressedData, type);
-            } else {
+              helper.loader.hide();
+              api.test = compressedData;
+          } else {
+            var errorMessage = document.getElementById("errorMessage");
+            errorMessage.classList.remove('hidden');
+            errorMessage.classList.add('show');
+            setTimeOut(function(){
+              errorMessage.classList.remove('show');
+              errorMessage.classList.add('hidden')
+            }, 3000)
+          }
+        };
+        request.onerror = function() {
+          console.log('error');
+        };
+        request.send();
+    },
+    characters: function () {
+      this.retrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=100', 'list')
+    },
+    character: function (id) {
+      this.retrieve(`http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05`, 'detail')
+    },
+    filter: function () {
+      this.filterRetrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=100')
+    },
+    filterRetrieve: function (url) {
+      helper.loader.show();
+        var request = new XMLHttpRequest();
+        request.open('GET', `${url}` , true);
+        request.onload = function() {
+          if (request.status >= 200 && request.status < 400) {
               var data = JSON.parse(request.responseText);
               var compressedData = data.data.results;
-              console.log('api' + userInput);
-              feature.init(compressedData, userInput);
-            }
-
+              helper.loader.hide();
           } else {
             console.log(request.status);
           }
@@ -130,15 +182,6 @@
           console.log('error');
         };
         request.send();
-    },
-    characters () {
-      this.retrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=100', 'list')
-    },
-    character (id) {
-      this.retrieve(`http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05`, 'detail')
-    },
-    filter (userInput) {
-      this.retrieve('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=e2c13dcb787436182cc25b65cbcfde95&hash=f60cbe16481792de29bc9c79da37aa05&limit=100', 'filter', userInput)
     }
   }
   app.init();
